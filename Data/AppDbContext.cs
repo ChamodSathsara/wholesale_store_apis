2f88270517f6cabefeef5243e6875309bdaaf9e0
ChamodSathsara <sathsarawapc@gmail.com>
2025-11-05 16:50:17 +0530

    set Connection strings

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using wholesale_store.Models.Entities;
+
+namespace wholesale_store.Data
+{
+    public class AppDbC :DbContext
+    {
+        public AppDbC(DbContextOptions<AppDbC> options):base(options) 
+        {
+            
+        }
+        public DbSet<Tasks> Tasks { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using wholesale_store.Data;
 using wholesale_store.Servicess;
 using wholesale_store.Servicess.Repositories;
 using wholesale_store.Servicess.Servicess;
@@ -10,7 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ITasksRepository,TaskService>();
+//builder.Services.AddScoped<ITasksRepository,TaskService>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
